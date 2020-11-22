d5c2408d6c810d4f29b198dc06aa1e564c4cac1c
yuta <az7xj5xn@yahoo.co.jp>
2020-11-22 23:27:36 +0900

    投稿機能を追加しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,19 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+  end
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(params.require(:post).permit(:content))
+    if @post.save
+      redirect_to posts_path
+    else render 'new'
+    end
   end
 
   def edit
